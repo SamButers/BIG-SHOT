138aa25af4526fc6a50d554cb276b6ef1f75e106
Sam Buters <sambuters@gmail.com>
2023-04-11 18:58:09 -0300

    Adding fonts and setting up tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,32 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          300: '#E4628E',
+          500: '#FFAEC9',
+          700: '#FFDAE6'
+        },
+        secondary: {
+          300: '#B1AB27',
+          500: '#FFF301',
+          700: '#FFF868'
+        }
       },
+
+      fontFamily: {
+        'blazma': ['Blazma', 'sans-serif'],
+        'glametrix': ['Glametrix', 'sans-serif']
+      },
+
+      screens: {
+        'sm-lg': {'raw': '(min-width: 768px) and (max-width: 991px) and (min-aspect-ratio: 1/1)'},
+        'md': {'raw': '(min-width:  768px) and (max-aspect-ratio: 1/1)'},
+        'lg': '992px',
+        '3xl': '1920px',
+        '4xl': '3840px',
+        '5xl': '7680px'
+      }
     },
   },
   plugins: [],
