df9570b36906c0e6082e0a8f1ae3513744b4e7d8
Sam Buters <sambuters@gmail.com>
2023-04-20 05:00:26 -0300

    Adding mobile and desktop responsiveness up to 4k to Page 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,10 +4,26 @@ class Page404 extends React.Component {
 	render() {
 		return (
 			<main className="w-full h-screen bg-black relative overflow-hidden font-8bitoperator text-white" ref={this.mainElement}>
-				<span className={`absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-[16rem] leading-[16rem] opacity-10 select-none`}>404</span>
+				<span className={`absolute left-1/2 -translate-x-1/2 opacity-10 select-none
+					top-1/2 -translate-y-1/2 text-[16rem] leading-[16rem]
+					sm:top-0 sm:translate-y-0 sm:leading-[9rem] sm:mt-4
+					md:top-1/2 md:-translate-y-1/2 md:leading-[16rem] md:mt-0
+					lg:top-1/2 lg:-translate-y-1/2 lg:leading-[16rem] lg:mt-0
+					4xl:text-[32rem] 4xl:leading-[32rem]
+				`}>404</span>
 
-				<div className={`w-[85%] aspect-[6.23] absolute bottom-0 left-1/2 -translate-x-1/2 border-8 border-white mb-8 px-4 py-2 select-none`}>
-					<p className={`text-[2rem] leading-[2rem]`}>* But nobody came.</p>
+				<div className={`absolute bottom-0 left-1/2 -translate-x-1/2 bg-black border-white select-none
+					w-[90%] aspect-[2.5] border-[6px] mb-4 px-4 py-2
+					sm:aspect-[4.23]
+					md:aspect-[4.23] md:border-8 md:mb-8
+					lg:w-[90%] lg:aspect-[5.23] lg:border-8 lg:mb-8
+					xl:w-[85%] xl:aspect-[6.23]
+					4xl:border-[16px] 4xl:px-8 4xl:py-4
+				`}>
+					<p className={`
+						text-[2rem] leading-[2rem]
+						4xl:text-[4rem] 4xl:leading-[4rem]
+					`}>* But nobody came.</p>
 				</div>
 			</main>
 		)
